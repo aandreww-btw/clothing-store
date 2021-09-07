481960d5ac5af83daeba77adb6c83f8a00599a1e
anga0718 <andrei.galochkin@netcracker.com>
2021-09-07 12:24:17 +0400

    use react-router-dom to navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import {Route, Switch} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
