aef1e96fc4200015deb8237822c7aa65f62e2807
anga0718 <andrei.galochkin@netcracker.com>
2021-09-07 22:16:55 +0400

    create header + add authentication via google account with firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsmvuJBPNom8wuKyekf2De11b8KZAdtKc",
+    authDomain: "crwb-db-e4f85.firebaseapp.com",
+    projectId: "crwb-db-e4f85",
+    storageBucket: "crwb-db-e4f85.appspot.com",
+    messagingSenderId: "980200619129",
+    appId: "1:980200619129:web:44191649dc1684869568a8",
+    measurementId: "G-V78ZE888BJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
